e705ba2c8f1f970f7c25aa7cf1fdf568855b7d3e
EugeniuPeruzzi <jaka1997@live.it>
2023-07-10 22:30:10 +0200

    exercise fixed

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
     //  !! ATTENZIONE nella dichiarazione della variabile di new food il peso e ultimo come stringa invece nella struttura HTML e messo come pen ultimo !!
     
     // prodotti cibo
-    $food_1 = new Food('https://petworld-cdn-media.media-tinyelephant.no/pub/media/catalog/product/cache/32817bdccd358503519584bdb6c86f4d/3/1/3182550831383.jfif.jpg', 'Royal Canin Mini Adult', '<i class="fa-solid fa-dog"></i> Cane' , '$ 43,99' ,'prosciutto, riso', 'Peso netto: 545g');
+    $food_1 = new Food('https://petworld-cdn-media.media-tinyelephant.no/pub/media/catalog/product/cache/32817bdccd358503519584bdb6c86f4d/3/1/3182550831383.jfif.jpg', 'Royal Canin Mini Adult', '<i class="fa-solid fa-dog"></i> Cane' , '$ 43,99' ,'prosciutto, riso', '545g');
     $food_2 = new Food('https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg', 'Almo Naturale Holistic Maintenance Medium Large Tonno e Riso', '<i class="fa-solid fa-dog"></i> Cane' , '$ 44.99' ,'manzo, cereali', '600g');
     $food_3 = new Food('https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg', 'Almo Naturale Cat Daily Lattina', '<i class="fa-solid fa-cat"></i> Gatto' , '$ 34.99' ,'tonno, pollo, prosciutto', '400g');
     $food_4 = new Food('https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg', 'Mangime per Pesci Guppy in Fiocchi', '<i class="fa-solid fa-fish-fins"></i> Pesce' , '$ 2,95' ,'Pesci e sottoprodotti di pesci, ceriali, Lieviti, Alghe', '30g');
--- index.php
+++ index.php
@@ -23,47 +23,64 @@
     <title>PhP OOP 2 </title>
 </head>
 
+
 <body>
-    <div class="">
-        <!-- Food -->
-        <?php foreach ($food as $index => $show) { ?>
-            <div class="img-container">
-                <img src="<?php echo $show->img; ?>" alt="">
-            </div>
-            <h4><?php echo $show->title; ?></h4>
-            <h6><?php echo $show->animal_type; ?></h6>
-            <h6><?php echo $show->price; ?></h6>
-            <h6><?php echo $show->weight; ?></h6>
-            <h6><?php echo $show->description; ?></h6>
-        <?php } ?>
-    </div>
+    <div class="container">
+        <div class="row">
+            <h1 class="ms-2 mb-5">Boolshop</h1>
+            <h2 class="ms-2 mt-5">I nostri prodotti</h2>
+            <div class="col-10 d-flex flex-wrap">
+                <!-- Food -->
+                <?php foreach ($food as $index => $show) { ?>
+                    <div class="card m-2" style="width: 21rem;">
+                        <div class="img-container">
+                            <img src="<?php echo $show->img; ?>" alt="" class="card-img-top">
+                        </div>
+                        <div class="card-body">
+                            <h4><?php echo $show->title; ?></h4>
+                            <h6 class="mb-3"><?php echo $show->animal_type; ?></h6>
+                            <div class="my-1">Prezzo: <strong><?php echo $show->price; ?></strong></div>
+                            <div class="my-1">Peso netto: <?php echo $show->weight; ?></div>
+                            <div class="my-1">Ingredienti: <?php echo $show->description; ?></div>
+                        </div>
+                    </div>
+                <?php } ?>
+                
+                
+                <!-- Utility -->
+                <?php foreach ($utility as $index => $show) { ?>
+                    <div class="card m-2" style="width: 21rem;">
+                        <div class="img-container">
+                            <img src="<?php echo $show->img; ?>" alt="" class="card-img-top">
+                        </div>
+                        <div class="card-body">
+                            <h4><?php echo $show->title; ?></h4>
+                            <h6 class="mb-3"><?php echo $show->animal_type; ?></h6>
+                            <div class="my-1">Prezzo: <strong><?php echo $show->price; ?></strong></div>
+                            <div class="my-1">Materiale: <?php echo $show->description; ?></div>
+                            <div class="my-1">Dimensioni: <?php echo $show->dimension; ?></div>
+                        </div>
+                    </div>
+                <?php } ?>
+            
+                <!-- Games -->
+                <?php foreach ($games as $index => $show) { ?>
+                    <div class="card m-2" style="width: 21rem;">
+                        <div class="img-container">
+                            <img src="<?php echo $show->img; ?>" alt="" class="card-img-top">
+                        </div>
+                        <div class="card-body">
+                            <h4><?php echo $show->title; ?></h4>
+                            <h6 class="my-3"><?php echo $show->animal_type; ?></h6>
+                            <div class="my-1">Prezzo: <strong><?php echo $show->price; ?></strong></div>
+                            <div class="my-1">Carratteristiche: <?php echo $show->description; ?></div>
+                            <div class="my-1">Dimensioni: <?php echo $show->dimension_games; ?></div>
+                        </div>
+                    </div>
+                <?php } ?>
 
-    <div class="">
-        <!-- Utility -->
-        <?php foreach ($utility as $index => $show) { ?>
-            <div class="img-container">
-                <img src="<?php echo $show->img; ?>" alt="">
             </div>
-            <h4><?php echo $show->title; ?></h4>
-            <h6><?php echo $show->animal_type; ?></h6>
-            <h6><?php echo $show->price; ?></h6>
-            <h6><?php echo $show->description; ?></h6>
-            <h6><?php echo $show->dimension; ?></h6>
-        <?php } ?>
-    </div>
-
-    <div class="">
-        <!-- Games -->
-        <?php foreach ($games as $index => $show) { ?>
-            <div class="img-container">
-                <img src="<?php echo $show->img; ?>" alt="">
-            </div>
-            <h4><?php echo $show->title; ?></h4>
-            <h6><?php echo $show->animal_type; ?></h6>
-            <h6><?php echo $show->price; ?></h6>
-            <h6><?php echo $show->description; ?></h6>
-            <h6><?php echo $show->dimension_games; ?></h6>
-        <?php } ?>
+        </div>
     </div>
 </body>
 
